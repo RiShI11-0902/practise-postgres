62d78f7e6e9c31bbef052f36b541ab24ca4298e6
Rushikesh Bagade <rbagade911@gmail.com>
2025-02-23 19:13:42 +0530

    added the routing in index file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
 const app = express()
+const transaction_router = require("./router/transactionRouter")
+
+app.use("/course-transactions", transaction_router)
 
 app.get("/", async (req,res)=>{
-    res.json({message:"Listening on 500"})
+    res.json({message:"Listening on 5000"})
 })
 
 app.listen(5000)
