51fb05e8b6a467a762ee55664f7b4e9037637d32
Rushikesh Bagade <rbagade911@gmail.com>
2025-02-23 11:21:10 +0530

    added controller function in routes

--- router/transactionRouter.js
+++ router/transactionRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const transController = require('../controllers/tanscation')
 
-router.get("/last_6_month_tran")
+router.get("/last_6_month_tran", transController.last_six_month_trans)
 
 module.exports = router
