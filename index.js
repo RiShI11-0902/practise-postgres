de0fe9671f5046535e2dba2525119d7cbc68f4cd
Rushikesh Bagade <rbagade911@gmail.com>
2025-02-23 11:16:37 +0530

    created routing folder and file

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express')
 
+const app = express()
+
+app.get("/", async (req,res)=>{
+    res.json({message:"Listening on 500"})
+})
+
+app.listen(5000)
---
+++ router/transactionRouter.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/last_6_month_tran")
+
+module.exports = router
